aa261c2d9af924e8542c168aafb0c617b4a833e9
yoshiteru.nakasaka <yoshiteru0118@gmail.com>
2019-04-03 10:28:20 +0000

    users#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,29 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_parms)
+    
+    if @user.save
+      flash[:success] = "ユーザを登録しました。"
+      redirect_to @user
+    else
+      flash.now[:danger] = "ユーザ登録に失敗しました。"
+      render :new
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password,:password_confirmation)
   end
 end
+
